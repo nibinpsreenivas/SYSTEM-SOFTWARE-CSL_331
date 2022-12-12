986b36c8f97ed7f2498df0d0ef22e48b82d9b130
NIBIN <96453329+nibinpsreenivas@users.noreply.github.com>
2022-12-12 10:20:24 +0530

    Update single_pass.c

--- cycle 3/single pass/single_pass.c
+++ cycle 3/single pass/single_pass.c
@@ -3,6 +3,56 @@
     #include<stdlib.h>
     
     #include<string.h>
+char* reverse(char *buffer, int i, int j)
+{
+    while (i < j) {
+        swap(&buffer[i++], &buffer[j--]);
+    }
+ 
+    return buffer;
+}
+ 
+
+char* itoa(int value, char* buffer, int base)
+{
+    
+    if (base < 2 || base > 32) {
+        return buffer;
+    }
+ 
+   
+    int n = abs(value);
+ 
+    int i = 0;
+    while (n)
+    {
+        int r = n % base;
+ 
+        if (r >= 10) {
+            buffer[i++] = 65 + (r - 10);
+        }
+        else {
+            buffer[i++] = 48 + r;
+        }
+ 
+        n = n / base;
+    }
+ 
+    
+    if (i == 0) {
+        buffer[i++] = '0';
+    }
+ 
+
+    if (value < 0 && base == 10) {
+        buffer[i++] = '-';
+    }
+ 
+    buffer[i] = '\0'; 
+ 
+   
+    return reverse(buffer, 0, i - 1);
+}
     void main()
     {
        FILE *f1,*f2,*f3,*f4,*f5;
